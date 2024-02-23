95a9f957bf6c40b0b0bf759c5cf2ca2e5edba8db
Sufyanjunani <sasifjunani@gmail.com>
2024-02-23 15:00:11 +0500

    add my alert assignment

---
+++ app.js
@@ -0,0 +1,56 @@
+// chapter (1)ALERT
+var firstname = "Usman"
+var lastname = "Asif"
+var email = "Usmanjunai@gmail.com"
+var number = "03178960796"
+// Q1
+alert(firstname)
+alert(lastname)
+alert(email)
+alert(number)
+// Q2
+alert("You're learning javascript!")
+alert("money always win")
+
+// chapter 2
+
+// q1
+var camelCase;
+
+// q2
+var thisisvariablename;
+var thisisvariablename = "this is a value "
+
+// q3
+var teamname = "coding team"
+alert(teamname)
+
+// q4
+var player = "babar"
+var player = "rizwan"
+
+// chap 3
+
+// q1
+var gold
+
+// q2
+var gold = 144
+
+// q3
+var num = 9
+
+// q4
+var b;
+var b = 5 + 5
+
+// q5
+var merchtotal = 100;
+var charges = 10;
+var ordertotal = merchtotal + charges;
+console.log(ordertotal)
+
+// q6
+var n = 2;
+var n = n + 2
+console.log(n)
